279c7eb41ad49582b2d8c27f5e52930ab13f7d29
Juuso Paakkunainen <85946482+paakkis@users.noreply.github.com>
2022-06-19 01:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from faceitstats import Stats
 API_KEY = ""
 player = "myy"
 
-faceitmyy = Stats(API_KEY, player)
-faceitmyy.get_player_id()
-faceitmyy.get_players_stats()
-faceitmyy.get_all_match_stats()
-faceitmyy.dump()
+faceit = Stats(API_KEY, player)
+faceit.get_player_id()
+faceit.get_players_stats()
+faceit.get_all_match_stats()
+faceit.dump()
